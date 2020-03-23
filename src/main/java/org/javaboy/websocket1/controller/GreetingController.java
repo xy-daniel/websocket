0b1209cdfeb89d70c9aa98a6a28195b685fc128c
damiel <daniel@users.noreply.github.com>
2020-03-23 17:09:26 +0800

    websocket订阅群发的第二种写法

--- src/main/java/org/javaboy/websocket1/controller/GreetingController.java
+++ src/main/java/org/javaboy/websocket1/controller/GreetingController.java
@@ -2,22 +2,35 @@ package org.javaboy.websocket1.controller;
 
 import org.javaboy.websocket1.bean.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
 
 @Controller
 public class GreetingController {
 
+    @Resource
+    SimpMessagingTemplate simpMessagingTemplate;
+
+//    /**
+//     * 通过/hello接收消息后转发到/topic/greetings
+//     * @param message  接收的消息
+//     * @return 返回的消息
+//     */
+//    @MessageMapping("/hello")
+//    @SendTo("/topic/greetings")
+//    public Message greeting(Message message){
+//        return message;
+//    }
+
     /**
-     * 通过/hello接收消息后转发到/topic/greetings
-     * @param message  接收的消息
-     * @return 返回的消息
+     * 群发的第二种学法
+     * @param message  接收到的消息
      */
     @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Message greeting(Message message){
-        return message;
+    public void greeting(Message message){
+        simpMessagingTemplate.convertAndSend("/topic/greetings", message);
     }
 
 }
