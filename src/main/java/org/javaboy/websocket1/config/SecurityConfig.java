c543c24fe6bdfeaab64f670d96abf2257b29ee92
damiel <daniel@users.noreply.github.com>
2020-03-23 17:16:18 +0800

    websocket点对点发送需要security支持以引入用户的概念

---
+++ src/main/java/org/javaboy/websocket1/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package org.javaboy.websocket1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 取消密码加密
+     * @return
+     */
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("javaboy").password("123").roles("admin")
+                .and()
+                .withUser("sang").password("123").roles("user");
+    }
+}
